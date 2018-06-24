45a24f3aded7a3030b7fd5a0b97940883adc316e
SpacezCowboy <40532691+SpacezCowboy@users.noreply.github.com>
2018-06-24 13:51:33 -0500

    Updates
    Updae Powershell Profile and Get-Foldersize

--- PowerShell/Profiles-Modules/Profiles/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Profiles-Modules/Profiles/Microsoft.PowerShell_profile.ps1
@@ -2622,31 +2622,57 @@ Function Get-FolderSize {
     Quckly Finds the size of a single folder
         
     .INPUTS
-   [CmdletBinding(SupportsShouldProcess)]   
+    [CmdletBinding(SupportsShouldProcess)]   
     Param (
-        [Parameter(Position=0, Mandatory = $true)]$Folder
+        [Parameter(Position=0, Mandatory = $true)]$Folder,
+        [Parameter(Position=1)]$ByteSize
            )
                     
-      
-    "{0:N2} MB" -f ((Get-ChildItem $Folder -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / 1MB)
+    if ($ByteSize -eq 'MB'){ 
+        "{0:N2} MB" -f ((Get-ChildItem $Folder -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / 1MB)
+    } elseif ($ByteSize -eq 'GB') {
+        "{0:N2} GB" -f ((Get-ChildItem $Folder -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / 1GB)
+    } elseif ($ByteSize -eq 'TB') {
+        "{0:N2} TB" -f ((Get-ChildItem $Folder -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / 1TB)
+    } else {
+        "{0:N2} MB" -f ((Get-ChildItem $Folder -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / 1MB)
+    }
         
                 
     .EXAMPLE
-    Get-Foldersize -Folder c:\users\
+    Get-Foldersize -Folder c:\users\ -ByteSize MB
 
-    Finds the size of the C:\users folder    
+    Finds the size of the C:\users folder in MegaBytes
+    
+    .EXAMPLE
+    Get-Foldersize -Folder c:\users\ -ByteSize GB
+
+    Finds the size of the C:\users folder in GigaBytes
+
+    .EXAMPLE
+    Get-Foldersize -Folder c:\users\ -ByteSize TB
+
+    Finds the size of the C:\users folder in MegaBytes
     
     #>
         
     [CmdletBinding(SupportsShouldProcess)]   
     Param (
-        [Parameter(Position=0, Mandatory = $true)]$Folder
-           )
+        [Parameter(Position = 0, Mandatory = $true)]$Folder,
+        [Parameter(Position = 1)]$ByteSize
+    )
                     
-      
-    "{0:N2} MB" -f ((Get-ChildItem $Folder -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / 1MB)
-
-                        }
-
-
+    if ($ByteSize -eq 'MB') { 
+        "{0:N2} MB" -f ((Get-ChildItem $Folder -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / 1MB)
+    }
+    elseif ($ByteSize -eq 'GB') {
+        "{0:N2} GB" -f ((Get-ChildItem $Folder -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / 1GB)
+    }
+    elseif ($ByteSize -eq 'TB') {
+        "{0:N2} TB" -f ((Get-ChildItem $Folder -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / 1TB)
+    }
+    else {
+        "{0:N2} MB" -f ((Get-ChildItem $Folder -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / 1MB)
+    }
+}
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
